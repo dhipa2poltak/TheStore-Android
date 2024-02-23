7115ace81e709ca920f58898a2baeccfd2a44c17
Dhipa Poltak <dhipa2poltak@gmail.com>
2024-02-23 16:58:24 +0700

    adding unit test for NetworkStateDataSource (implementation)

---
+++ framework/src/test/java/com/dpfht/thestore/framework/data/datasource/local/NetworkStateDataSourceTest.kt
@@ -0,0 +1,40 @@
+package com.dpfht.thestore.framework.data.datasource.local
+
+import com.dpfht.thestore.data.datasource.NetworkStateDataSource
+import com.dpfht.thestore.framework.util.net.OnlineChecker
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class NetworkStateDataSourceTest {
+
+  private lateinit var networkStateDataSource: NetworkStateDataSource
+
+  @Mock
+  private lateinit var onlineChecker: OnlineChecker
+
+  @Before
+  fun setup() {
+    networkStateDataSource = NetworkStateDataSourceImpl(onlineChecker)
+  }
+
+  @Test
+  fun `ensure isOnline true`() {
+    whenever(onlineChecker.isOnline()).thenReturn(true)
+
+    assertTrue(networkStateDataSource.isOnline())
+  }
+
+  @Test
+  fun `ensure isOnline false`() {
+    whenever(onlineChecker.isOnline()).thenReturn(false)
+
+    assertFalse(networkStateDataSource.isOnline())
+  }
+}
